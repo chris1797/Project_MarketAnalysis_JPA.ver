891004c7b3dcf207faa3444b9d0c6860f6bdad62
chris1797 <jaehun6004@gmail.com>
2022-07-18 23:39:58 +0900

    2022-07-18 VO 생성

---
+++ src/main/java/com/project/model/Board_VO.java
@@ -0,0 +1,15 @@
+package com.project.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Board_VO {
+	
+	int num;
+	String author;
+	String title;
+	String contents;
+	java.sql.Date wdate;
+	int pcode;
+
+}
---
+++ src/main/java/com/project/model/User_VO.java
@@ -0,0 +1,87 @@
+package com.project.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class User_VO {
+	
+	String name;
+	java.sql.Date birth;
+	String uid;
+	String upw;
+	int phoneNum;
+	String nickName;
+	String gender;
+	String city;
+	String town;
+	String village;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public java.sql.Date getBirth() {
+		return birth;
+	}
+	public void setBirth(java.sql.Date birth) {
+		this.birth = birth;
+	}
+	public String getUid() {
+		return uid;
+	}
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+	public String getUpw() {
+		return upw;
+	}
+	public void setUpw(String upw) {
+		this.upw = upw;
+	}
+	public int getPhoneNum() {
+		return phoneNum;
+	}
+	public void setPhoneNum(int phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+	public String getNickName() {
+		return nickName;
+	}
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getTown() {
+		return town;
+	}
+	public void setTown(String town) {
+		this.town = town;
+	}
+	public String getVillage() {
+		return village;
+	}
+	public void setVillage(String village) {
+		this.village = village;
+	}
+	public String getManager() {
+		return manager;
+	}
+	public void setManager(String manager) {
+		this.manager = manager;
+	}
+	String manager;
+}
