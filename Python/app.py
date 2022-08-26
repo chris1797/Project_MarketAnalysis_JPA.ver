45f1928871f14808daea4c61d2ebe01afd3c0eca
chris1797 <98400863+chris1797@users.noreply.github.com>
2022-08-26 18:05:51 +0900

    Add files via upload

--- Python/app.py
+++ Python/app.py
@@ -3,10 +3,16 @@ import cx_Oracle
 import matplotlib as mpl # 그래프 옵션
 import matplotlib.pyplot as plt # 그래프 출력
 
+
+cx_Oracle.init_oracle_client(lib_dir="/Users/ijaehun/Downloads/instantclient_19_8")
+connection = cx_Oracle.connect(user="admin", password="Chldudcjf12!", dsn="db20220709105045_low")
+cursor = connection.cursor()
+
 # 1. 터미널에서 flask run 실행
 # 2. http://127.0.0.1:5000 주소창에서 접속
 
 app = Flask(__name__)
+
 
 # localhost/home 연결 시 flask_server.html 로 연결
 @app.route('/')
